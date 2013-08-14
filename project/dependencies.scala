4078b78c0413b5e5d74f576811576d9fb4282170
Christian Krause <kizkizzbangbang@googlemail.com>
2013-08-14 21:11:54 +0200

    sbt and scalaz version bump

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 
 object Dependencies {
   val scalacheck = "org.scalacheck" %% "scalacheck"  % "1.10.1"
-  val scalaz     = "org.scalaz"     %% "scalaz-core" % "7.0.2"
+  val scalaz     = "org.scalaz"     %% "scalaz-core" % "7.0.3"
   val specs2     = "org.specs2"     %% "specs2"      % "2.1.1"
   val spire      = "org.spire-math" %% "spire"       % "0.5.1"
 }
