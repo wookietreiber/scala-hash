259d11aebde4fe0892ca1ac33a18ac83dad28782
Christian Krause <kizkizzbangbang@googlemail.com>
2013-12-27 15:09:22 +0100

    adds missing override to members

--- contrib/scalaz/main/scala/Adler32.scala
+++ contrib/scalaz/main/scala/Adler32.scala
@@ -10,8 +10,8 @@ object adler32 extends Adler32Instances
 trait Adler32Instances {
 
   implicit val Adler32CombinationMonoid: Monoid[(Adler32,Int)] = new Monoid[(Adler32,Int)] {
-    val zero = (Adler32(),0)
-    def append(x: (Adler32,Int), y: ⇒ (Adler32,Int)): (Adler32,Int) = {
+    override val zero = (Adler32(),0)
+    override def append(x: (Adler32,Int), y: ⇒ (Adler32,Int)): (Adler32,Int) = {
       (x._1.combine(y._1, y._2), x._2 + y._2)
     }
   }
--- contrib/scalaz/main/scala/CRC32.scala
+++ contrib/scalaz/main/scala/CRC32.scala
@@ -10,8 +10,8 @@ object crc32 extends CRC32Instances
 trait CRC32Instances {
 
   implicit val CRC32CombinationMonoid: Monoid[(CRC32,Int)] = new Monoid[(CRC32,Int)] {
-    val zero = (CRC32(),0)
-    def append(x: (CRC32,Int), y: ⇒ (CRC32,Int)): (CRC32,Int) = {
+    override val zero = (CRC32(),0)
+    override def append(x: (CRC32,Int), y: ⇒ (CRC32,Int)): (CRC32,Int) = {
       (x._1.combine(y._1, y._2), x._2 + y._2)
     }
   }
