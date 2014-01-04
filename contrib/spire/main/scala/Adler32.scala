bbfb996874e1eab4c03f59dc8b761d8849284b95
Christian Krause <kizkizzbangbang@googlemail.com>
2014-01-04 14:37:13 +0100

    spire eq instances

--- contrib/spire/main/scala/Adler32.scala
+++ contrib/spire/main/scala/Adler32.scala
@@ -3,11 +3,16 @@ package hash
 
 import scalax.hash.Adler32
 
-import spire.algebra.Monoid
+import spire.algebra._
 
 object adler32 extends Adler32Instances
 
 trait Adler32Instances {
+
+  implicit val Adler32Eq: Eq[Adler32] = new Eq[Adler32] {
+    override def eqv(x: Adler32, y: Adler32): Boolean =
+      x == y
+  }
 
   implicit val Adler32Monoid: Monoid[Adler32] = new Monoid[Adler32] {
     override val id = Adler32.empty
--- contrib/spire/main/scala/CRC32.scala
+++ contrib/spire/main/scala/CRC32.scala
@@ -3,11 +3,16 @@ package hash
 
 import scalax.hash.CRC32
 
-import spire.algebra.Monoid
+import spire.algebra._
 
 object crc32 extends CRC32Instances
 
 trait CRC32Instances {
+
+  implicit val CRC32Eq: Eq[CRC32] = new Eq[CRC32] {
+    override def eqv(x: CRC32, y: CRC32): Boolean =
+      x == y
+  }
 
   implicit val CRC32Monoid: Monoid[CRC32] = new Monoid[CRC32] {
     override val id = CRC32.empty
